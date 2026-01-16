6e595dbe2f1ba4c89144104f27bfbc3a7d5de89e
Isanmar0910 <isanmar0910@g.educaand.es>
2026-01-16 08:33:17 +0100

    dadadaw

--- Tema6/Ejercicios/Ejercicio6.java
+++ Tema6/Ejercicios/Ejercicio6.java
@@ -8,6 +8,8 @@ public class Ejercicio6 {
 
         int[] numeros = new int[MAXIMO];
 
+        int temporal;
+
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Vaya introduciendo numeros enteros y pulsando INTRO");
@@ -17,37 +19,34 @@ public class Ejercicio6 {
         }
 
         System.out.println("Array original:");
-
         for (int i = 0; i < 15; i++) {
-            System.out.printf("|%5d", i);
-
+            System.out.printf("|%3d ", i);
         }
-        System.out.println("");
-        System.out.println(
-                "--------------------------------------------------------------------------------------------");
+       
 
+        System.out.println("-");
         for (int i = 0; i < 15; i++) {
-
-            System.out.printf("|%5d", numeros[i]);
+            System.out.printf("|%3d ", numeros[i]);
         }
+        System.out.println("|");
 
+       
+        temporal = numeros[14];
+        for (int i = 14; i > 0; i--) {
+            numeros[i] = numeros[i - 1];
+        }
+        numeros[0] = temporal;
+
+        System.out.println("\nArray rotado a la derecha una posición:");
         for (int i = 0; i < 15; i++) {
-            System.out.printf("|%5d", i);
-            
+            System.out.printf("|%3d ", i);
         }
-
-        System.out.println("");
-        System.out.println(
-                "--------------------------------------------------------------------------------------------");
-
+        
+        System.out.println("-");
         for (int i = 0; i < 15; i++) {
-            
-            if (i == (MAXIMO - 1)) {
-                
-            }
-            
-
+            System.out.printf("|%3d ", numeros[i]);
         }
+        System.out.println("|");
 
     }
 }
