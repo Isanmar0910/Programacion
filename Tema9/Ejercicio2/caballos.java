d915df27a801f70c2901ad1d9d89e8bfb1255f58
Isanmar0910 <isanmar0910@g.educaand.es>
2026-02-05 14:39:27 +0100

    ssrwre

---
+++ Tema9/Ejercicio2/caballos.java
@@ -0,0 +1,23 @@
+package Tema9.Ejercicio2;
+
+public class caballos {
+
+    String nombre;
+    String sexo;
+    String raza;
+
+    double peso;
+    int edad;
+
+    caballos (String nombre){
+        this.nombre = nombre;
+    }
+
+    void trota() {
+        System.out.println("toctoc toc toctoc");
+    }
+
+    
+
+    
+}
